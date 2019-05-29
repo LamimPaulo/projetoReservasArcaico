baf8c011c7d44bc0feb4201b6396b39588aa3c22
Paulo Lamim <robertolamim@gmail.com>
2019-05-29 16:06:32 -0300

    finalizado v1.0

--- calendario.php
+++ calendario.php
@@ -1,18 +1,18 @@
 <?php
 
-$data = '2019-06';
+$data = $_GET['ano'] . '-' . $_GET['mes'];
 $dia1 = date('w', strtotime($data));
 $dias = date('t', strtotime($data));
-$linhas = ceil(($dia1+$dias) / 7);
+$linhas = ceil(($dia1 + $dias) / 7);
 $dia1 = -$dia1;
-$data_inicio = date('Y-m-d', strtotime($dia1.' days', strtotime($data)));
-$data_fim = date('Y-m-d', strtotime(( ($dia1 + ($linhas*7) - 1 ) ).' days', strtotime($data)));
+$data_inicio = date('Y-m-d', strtotime($dia1 . ' days', strtotime($data)));
+$data_fim = date('Y-m-d', strtotime((($dia1 + ($linhas * 7) - 1)) . ' days', strtotime($data)));
 
-echo "primeiro dia: ".$dia1."<br>";
-echo "total de Dias: ".$dias."<br>";
-echo "Total de Linhas: ".$linhas."<br>";
-echo "Data inicio: ".$data_inicio."<br>";
-echo "Data fim: ".$data_fim."<br>";
+echo "primeiro dia: " . $dia1 . "<br>";
+echo "total de Dias: " . $dias . "<br>";
+echo "Total de Linhas: " . $linhas . "<br>";
+echo "Data inicio: " . $data_inicio . "<br>";
+echo "Data fim: " . $data_fim . "<br>";
 ?>
 
 <table border="1" width="100%">
@@ -25,13 +25,26 @@ echo "Data fim: ".$data_fim."<br>";
         <th>Sex</th>
         <th>Sab</th>
     </tr>
-    <?php for($l=0;$l<$linhas;$l++): ?>
+    <?php for ($l = 0; $l < $linhas; $l++) : ?>
         <tr>
-            <?php for($q=0;$q<7;$q++): ?>
-            <?php $w = date('d', strtotime(($q+($l*7)).' days',strtotime($data_inicio)) );
-             ?>
-            <td><?php echo $w; ?> </td>
+            <?php for ($q = 0; $q < 7; $q++) : ?>
+                <?php
+                $t = strtotime(($q + ($l * 7)) . ' days', strtotime($data_inicio));
+                $w = date('Y-m-d', $t);
+                ?>
+                <td><?php
+                    echo date('d', $t)."<br><br>";
+                    $w = strtotime($w);
+                    foreach ($lista as $item) {
+                        $dr_inicio = strtotime($item['data_inicio']);
+                        $dr_fim = strtotime($item['data_fim']);
+
+                        if ($w >= $dr_inicio && $w <= $dr_fim) {
+                            echo $item['pessoa']." (".$item['id_carro'].")"."<br>";
+                        }
+                    }
+                    ?></td>
             <?php endfor; ?>
         </tr>
-        <?php endfor; ?>
-    </table>
+    <?php endfor; ?>
+</table>
--- index.php
+++ index.php
@@ -1,25 +1,67 @@
-<?php 
+<?php
 require 'config.php';
 require 'classes/reservas.class.php';
 
-
 $reservas = new Reservas($pdo);
+
+
+
 ?>
 
 <a href="reservar.php">Adicionar reserva</a>
+<br><br>
+
+<form method="GET">
+    <select name="ano">
+        <?php for ($q = date('Y'); $q >= 2010; $q--) : ?>
+        <option><?php echo $q; ?></option>
+        <?php endfor; ?>
+    </select>
+    <select name="mes">
+        <option>01</option>
+        <option>02</option>
+        <option>03</option>
+        <option>04</option>
+        <option>05</option>
+        <option>06</option>
+        <option>07</option>
+        <option>08</option>
+        <option>09</option>
+        <option>10</option>
+        <option>11</option>
+        <option>12</option>
+    </select>
+    
+    <input type="submit" value='mostrar'>
+    
+</form>
+<?php
+if (empty($_GET['ano'])) {
+    exit;
+}
+
+$data = $_GET['ano'] . '-' . $_GET['mes'];
+$dia1 = date('w', strtotime($data));
+$dias = date('t', strtotime($data));
+$linhas = ceil(($dia1 + $dias) / 7);
+$dia1 = -$dia1;
+$data_inicio = date('Y-m-d', strtotime($dia1 . ' days', strtotime($data)));
+$data_fim = date('Y-m-d', strtotime((($dia1 + ($linhas * 7) - 1)) . ' days', strtotime($data)));
+?>
 
 
 <h1>Reservas</h1>
 <?php
-$lista = $reservas->getReservas();
-foreach($lista as $item) {
+$lista = $reservas->getReservas($data_inicio, $data_fim);
+/*
+foreach ($lista as $item) {
     $data1 = date('d/m/Y', strtotime($item['data_inicio']));
     $data2 = date('d/m/Y', strtotime($item['data_fim']));
-    echo $item['pessoa'].' Reservou o carro '.$item['id_carro'].' entre '.$data1.' e '.$data2.'</br>';
-}
+    echo $item['pessoa'] . ' Reservou o carro ' . $item['id_carro'] . ' entre ' . $data1 . ' e ' . $data2 . '</br>';
+}*/
 ?>
 
 <hr>
-<?php 
+<?php
 require 'calendario.php';
 ?>
